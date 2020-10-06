a38c8bcebc75dac57dba8586859b32bfc20e7631
Киселёв Дмитрий Андреевич <kiselyov.dmitrij@otr.ru>
2020-10-06 14:22:58 +0300

    done

--- Text_based_browser/browser.py
+++ Text_based_browser/browser.py
@@ -1,25 +1,55 @@
 # coding: utf-8
 import os
 import argparse
-from
-
+import requests
+# from .parsing import pars_site
+from bs4 import BeautifulSoup
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', type=str)
 key = parser.parse_args()
-os.makedirs(key.dir)
+os.makedirs(key.dir, exist_ok=True)
 
 
-def write_file(text, site):
-    site_dir = '/' + site
-    with open(key.dir + site_dir, 'w') as f:
-        f.write(text)
-    browser()
+def btf_soup(url, file_name):
+    global stak
+    reques = requests.get(url)
+    soup = BeautifulSoup(reques.content, "html.parser")
+    src = ('[document]', 'head', 'script', 'style',
+           'body', 'html', 'h1', 'h2', 'h3', 'h4',
+           'h5', 'h6', 'title', 'table', 'div', 'li', 'form')
+    tmp = soup.find_all(src)
+    file = open(key.dir + '/' + file_name, 'w', encoding='utf-8')
+    for i in tmp:
+        print(Fore.MAGENTA, i.text)
+        print(i.text, file=file)
+    file.close()
+
+    if flag != 0:
+        with open(key.dir + '/' + file_name, 'r', encoding='utf-8') as file:
+            stak.append(file.read())
+            return None
+    else:
+        return None
 
 
-def add_site_stak(text):
-    stak.append(text)
-    browser()
+def validation_url(url):
+    if 'http' not in url:
+        url = 'https://' + url
+    return url
+
+
+def name_file(url):
+    if 'https' in url:
+        file_name = url[8:]
+    else:
+        file_name = url[6:]
+    i = file_name.find('/')
+    file_name = file_name[0:i]
+    btf_soup(url, file_name)
 
 
 def print_site_stak():
@@ -28,7 +58,7 @@ def print_site_stak():
     elif len(stak) == 0:
         pass
     else:
-        print(stak[0])
+        print(Fore.CYAN, stak[0][0])
     browser()
 
 
@@ -37,17 +67,15 @@ def browser():
     while True:
         command = input()
         if '.' in command:
-            text = pars_url(command)
+            name_file(validation_url(command))
             if flag == 0:
                 flag += 1
-            else:
-                add_site_stak(text)
         elif "back" in command:
             print_site_stak()
         elif command == 'exit':
             exit()
         else:
-            print('Error: Incorrect URL')
+            print(Fore.RED, 'Error: Incorrect URL')
 
 
 flag = 0
