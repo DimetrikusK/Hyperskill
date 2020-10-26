9c161fe92e9527c83ccaf5f8beef95a3fb240a86
Киселёв Дмитрий Андреевич <kiselyov.dmitrij@otr.ru>
2020-10-26 15:23:48 +0300

    ToDoList done

--- To-Do List/test.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-
-today = datetime.today() - timedelta(days=20)
-tmp = today.strftime('%d')
-print(int(tmp))
-print(f'{today.strftime("%a")} {int(tmp)} {today.strftime("%b")}')
--- To-Do List/to-do_list.py
+++ To-Do List/to-do_list.py
@@ -21,40 +21,38 @@ class Table(Base):
 
 
 class ToDoList:
+    count = 1
 
     def __init__(self, task=None, my_date=None):
         self.task = task
+        self.today = datetime.today()
         if my_date:
             my_date = my_date.split('-')
             self.deadlines = datetime(int(my_date[0]), int(my_date[1]), int(my_date[2]))
 
     def today_tasks(self):
-        today = datetime.today()
-        rows = session.query(Table).filter(Table.deadline == today.date()).all()
-        count = 1
+        rows = session.query(Table).filter(Table.deadline == self.today.date()).all()
         if rows:
             for task in rows:
-                print(f'{count}. {task}')
-                count += 1
+                print(f'{self.count}. {task}\n')
+                self.count += 1
         else:
             print('Nothing to do!\n')
 
     def week_task(self):
-        today = datetime.today()
-        week = today.date() + timedelta(days=6)
-        day = today.date()
+        week = self.today.date() + timedelta(days=6)
+        day = self.today.date()
         rows = session.query(Table).order_by(Table.deadline).\
-            filter(Table.deadline >= today.date(), Table.deadline <= week).all()
-        count = 1
+            filter(Table.deadline >= self.today.date(), Table.deadline <= week).all()
         for i in range(7):
             if rows and day == rows[0].deadline:
                 print(day.strftime('%A %d %b:'))
                 for task in rows:
                     if day == task.deadline:
-                        print(f'{count}. {task}')
-                        count += 1
+                        print(f'{self.count}. {task}')
+                        self.count += 1
                 rows = rows[1:]
-                count= 1
+                self.count = 1
                 print('')
             else:
                 print(day.strftime('%A %d %b:'))
@@ -63,20 +61,50 @@ class ToDoList:
 
     def all_tasks(self):
         rows = session.query(Table).order_by(Table.deadline).all()
-        count = 1
         print('All tasks:')
         if rows:
             for task in rows:
                 day = task.deadline.strftime('%d')
-                print(f'{count}. {task.task}. {int(day)} {task.deadline.strftime("%b")}')
-                count += 1
+                print(f'{self.count}. {task.task}. {int(day)} {task.deadline.strftime("%b")}')
+                self.count += 1
+            print('')
         else:
             print("Nothing to do!\n")
+        self.count = 1
+
+    def missed_tasks(self):
+        rows = session.query(Table).filter(Table.deadline < datetime.today().date()).all()
+        for task in rows:
+            day = task.deadline.strftime('%d')
+            print(f'{self.count}. {task}. {int(day)} {task.deadline.strftime("%b")}')
+            self.count += 1
+        self.count = 1
+        print('')
 
     def add_task(self):
         new_task = Table(task=self.task, deadline=self.deadlines.date())
         session.add(new_task)
         session.commit()
+
+    def delete_task(self):
+        tmp = []
+        rows = session.query(Table).order_by(Table.deadline).all()
+        if rows:
+            print('Choose the number of the task you want to delete:')
+            for task in rows:
+                day = task.deadline.strftime('%d')
+                print(f'{self.count}. {task.task}. {int(day)} {task.deadline.strftime("%b")}')
+                tmp.append(f'{self.count}  {task.task}')
+                self.count += 1
+            say = int(input())
+            tmp = tmp[say - 1][1:].strip()
+            rows = session.query(Table).filter(Table.task == tmp).all()
+            session.delete(rows[0])
+            session.commit()
+            self.count = 1
+            print('The task has been deleted!')
+        else:
+            print("Nothing to do!\n")
 
 
 if __name__ == '__main__':
@@ -88,7 +116,9 @@ if __name__ == '__main__':
         print("""1) Today's tasks
 2) Week's tasks
 3) All tasks
-4) Add task
+4) Missed tasks
+5) Add task
+6) Delete task
 0) Exit""")
         command = int(input())
         if command == 1:
@@ -99,11 +129,15 @@ if __name__ == '__main__':
         elif command == 3:
             todo.all_tasks()
         elif command == 4:
+            todo.missed_tasks()
+        elif command == 5:
             task = input('Enter task\n')
             my_date = input('Enter deadline\n')
             todo = ToDoList(task, my_date)
             todo.add_task()
             print('The task has been added!\n')
+        elif command == 6:
+            todo.delete_task()
         elif command == 0:
             print('Bye!')
             exit()
