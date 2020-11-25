0226c8143c5b643dfed05ef74b71ab4577396e5c
Киселёв Дмитрий Андреевич <kiselyov.dmitrij@otr.ru>
2020-11-25 15:47:49 +0300

    step 3/5

--- Password Hacker/hack.py
+++ Password Hacker/hack.py
@@ -9,12 +9,12 @@ class Client:
         self.port = int(port)
         self.tmp_pass = tmp_pass
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self):
         with socket.socket() as client_socket:
             client_socket.connect((self.host, self.port))
-            for i in range(1, 10):
-                for j in itertools.product(self.tmp_pass, repeat=i):
-                    message = ''.join(j)
+            for word in password_tmp:
+                mixed_case = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in word)))
+                for message in mixed_case:
                     client_socket.send(message.encode())
                     response = client_socket.recv(1024)
                     if response.decode() == 'Connection success!':
@@ -22,8 +22,23 @@ class Client:
                         exit()
 
 
+class Password:
+    def __init__(self):
+        self.password_tmp = str()
+
+    def parsing_password(self):
+        with open('passwords.txt', 'r', encoding='utf-8')as file:
+            for password in file:
+                self.password_tmp += password
+            self.password_tmp = self.password_tmp.split('\n')
+            self.password_tmp = [x for x in self.password_tmp if x.isalpha()]
+
+        return self.password_tmp
+
+
 if __name__ == '__main__':
-    host, port = argv[1:]
-    tmp_pass = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    run = Client(host, port, tmp_pass)
+    host, port = input(), input()
+    password_tmp = Password()
+    password_tmp = password_tmp.parsing_password()
+    run = Client(host, port, password_tmp)
     run.__call__()
--- Password Hacker/test.py
+++ Password Hacker/test.py
@@ -1,10 +1,36 @@
 import itertools
 
-tmp = str()
-tmp_pass = 'abcdefghijklmnopqrstuvwxyz0123456789'
-for i in range(1, 100):
-    for j in itertools.product(tmp_pass, repeat=i):
-        tmp = ''.join(j)
-        if tmp == 'pass':
-            print(tmp)
-            break
+# tmp = str()
+# tmp_pass = 'abcdefghijklmnopqrstuvwxyz0123456789'
+# for i in range(1, 100):
+#     for j in itertools.product(tmp_pass, repeat=i):
+#         tmp = ''.join(j)
+#         if tmp == 'pass':
+#             print(tmp)
+#             break
+
+
+
+
+password_tmp = str()
+with open('file.txt', 'r', encoding='utf-8')as file:
+    for password in file:
+        password_tmp += password
+    password_tmp = password_tmp.split('\n')
+    password_tmp = [x for x in password_tmp if x.isalpha()]
+
+for word in password_tmp:
+    mixed_case = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in word)))
+    for i in mixed_case:
+        print(i)
+
+# words = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in password_tmp)))
+# for i in words:
+#     print(i)
+# for word in password_tmp:
+#     src = itertools.product(word.upper(), word.lower())
+# for i in src:
+#     print(i)
+
+
+
